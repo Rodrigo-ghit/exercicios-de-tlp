d58000471f487689d94c640236e8e1329a02a0f5
Rodrigo-ghit <rodrigoeliasc873@gmail.com>
2025-02-27 20:35:41 +0100

    Update main.py
    Exercício 2

---
+++ matrizes/ex12/main.py
@@ -0,0 +1,14 @@
+#determinante de uma matriz 2x2 nivel medio exercicio 2
+#fucao para calcular o determinante 
+def determ(matriz):
+    a, b, c, d = matriz[0][0], matriz[0][1], matriz[1][0], matriz[1][1]
+    return a * d - b * c
+    #preenchimento da matriz
+mat= [[0, 0], [0, 0]]
+for i in range(2):
+	for j in range(2):
+		mat[i][j]=int(input('digute o numero '))
+	print()
+  #exibir o determinante 
+resultado = determ(mat)
+print(f"O determinante da matriz é: {resultado}")
