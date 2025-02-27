f0be84e3293875c76d291ff284d712fbb23a8787
Rodrigo-ghit <rodrigoeliasc873@gmail.com>
2025-02-27 20:51:23 +0100

    Update main.py
    Exercício 1

---
+++ matrizes/ex12/main.py
@@ -0,0 +1,14 @@
+#exercicio numero 1 nive l basico
+# preenchendo a matriz 
+mat=[[0,0,0],[0,0,0],[0,0,0]]
+for i in range(3):
+	for j in range(3):
+		mat[i][j]=int(input('insura o numero '))
+	print()
+  # exibindo 
+for i in range(3):
+		for j in range(3):
+			print(mat[i][j],end=" ")	
+		print() 
+		
+		    
